b8b28e434ba418982d5d4cbdc316afd40835fb50
vickyrr24 <51054175+vickyrr24@users.noreply.github.com>
2019-05-27 21:12:16 +0530

    Create leap.py

---
+++ leap.py
@@ -0,0 +1,16 @@
+def is_leap(year):
+    leap = False
+    
+    if(year%4==0):
+        if(year%100!=0):
+            leap=True
+    if(year%100==0 and year%400==0):
+        leap=True
+    
+    return leap
+
+y=int(input())
+if(is_leap(y)):
+    print("yes")
+else:
+    print("no")
