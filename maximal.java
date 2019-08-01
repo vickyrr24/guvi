3fa306d901a76071acb48da7db724ba1725c63af
vickyrr24 <51054175+vickyrr24@users.noreply.github.com>
2019-08-01 15:08:24 +0530

    Update maximal.java

--- maximal.java
+++ maximal.java
@@ -1,1 +1,21 @@
+import java.util.Scanner
+public class test {
 
+    public static void main(String[] args) {
+
+        String input;
+        Scanner obj = new Scanner(system.in)
+        input = obj.nextLine();
+        String output = new String();
+
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = 0; j < output.length(); j++) {
+                if (input.charAt(i) != output.charAt(j)) {
+                    output = output + input.charAt(i);
+                }
+            }
+        }
+
+        System.out.println(output.length());
+
+    }
